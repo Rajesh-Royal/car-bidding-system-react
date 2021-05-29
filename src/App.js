de4d561b56d6cc688cb5978f4dd98f36e035cbcc
unknown <rajeshroyal896@gmail.com>
2021-05-29 13:15:13 +0530

    Prettier and eslint Integration with vscode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <h2 className="bg-red-500">Car bidding system TravClan</h2>
+      <div className="user">
+        <section>Testing Eslint and prettier integration</section>
+      </div>
     </div>
   );
 }
