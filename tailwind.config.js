038f64ce476e9ab38e8c54e47bf88b6a946a6209
unknown <rajeshroyal896@gmail.com>
2021-05-30 08:28:07 +0530

    react feather icons and tailwindcss-variables added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
       maxWidth: {
         "avatar-size": "2rem",
       },
@@ -14,5 +19,9 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwind-css-variables")({
+      colors: "color",
+    }),
+  ],
 };
