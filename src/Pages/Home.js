f653ceebd2c9d2a8ac0340ac04a8ae96ce0bd062
unknown <rajeshroyal896@gmail.com>
2021-05-30 08:43:01 +0530

    Page title component added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import { BiddersList } from "../Components/BiddersList/BiddersList";
 import Pagination from "../Components/BiddersList/components/Pagination";
 import { CustomerDataProviderContext } from "../Context/CustomerListContext";
 import Container from "../Container/Container";
+import { PageTitle } from "../Components/Typography/Titles";
 
 const Home = (props) => {
   const { value: customersList, setBiddingSortingOrder } = useContext(CustomerDataProviderContext);
@@ -34,32 +35,38 @@ const Home = (props) => {
 
   return (
     <Container>
-      <div className="flex justify-between items-center dark:text-gray-200">
-        <h2 className="text-center font-medium text-2xl">Bidders List</h2>
-        <div className="filters flex space-x-6 items-center">
-          <p className="font-medium mr-4">Filters</p>
-          <form>
-            <select
-              name="sort table"
-              className="border p-1 focus:outline-none dark:bg-gray-500 dark:text-gray-200"
-              onChange={(e) => customersInAscendingOrderByBidAmount(e.target.value)}>
-              <option value="maxbid">Maximum Bids</option>
-              <option value="minbid">Minimum Bids</option>
-            </select>
-          </form>
-          <div className="flex items-center mr-2">
-            <label htmlFor="toggleB" className="flex items-center cursor-pointer">
-              <div className="relative">
-                <input
-                  type="checkbox"
-                  id="toggleB"
-                  className="sr-only"
-                  onChange={(e) => setBiddingSortingOrder()}
-                />
-                <div className="block bg-gray-600 dark:bg-gray-500 w-10 h-6 rounded-full"></div>
-                <div className="dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition"></div>
-              </div>
-            </label>
+      <div className="overflow-auto">
+        <PageTitle className="text-center font-medium text-2xl">Bidders List</PageTitle>
+        <div className="flex flex-col items-center justify-between mb-2 px-4 py-2 bg-green-500 rounded-lg shadow-md md:flex-row">
+          <p className="text-white font-semibold">Filters</p>
+          <div className="filter-container flex items-center space-x-4">
+            <form>
+              <select
+                name="sort table"
+                className="p-1 focus:outline-none dark:text-gray-200 dark:bg-gray-500 bg-transparent text-white"
+                onChange={(e) => customersInAscendingOrderByBidAmount(e.target.value)}>
+                <option value="maxbid" className="text-gray-400 dark:text-gray-200">
+                  Maximum Bids
+                </option>
+                <option value="minbid" className="text-gray-400 dark:text-gray-200">
+                  Minimum Bids
+                </option>
+              </select>
+            </form>
+            <div className="flex items-center mr-2">
+              <label htmlFor="toggleB" className="flex items-center cursor-pointer">
+                <div className="relative">
+                  <input
+                    type="checkbox"
+                    id="toggleB"
+                    className="sr-only"
+                    onChange={(e) => setBiddingSortingOrder()}
+                  />
+                  <div className="block bg-gray-600 dark:bg-gray-500 w-10 h-6 rounded-full"></div>
+                  <div className="dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition"></div>
+                </div>
+              </label>
+            </div>
           </div>
         </div>
       </div>
